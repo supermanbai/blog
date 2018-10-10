f0514cdbac2782d805f3807d4de43d4a9a76df6c
supermanbai <webbaiemail@163.com>
2018-10-10 18:08:31 +0800

    Site updated: 2018-10-10 18:08:27

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "平凡之路",
+        artist: '朴树',
+        url: 'http://m10.music.126.net/20181010181008/feee125d73c0ea2de87f5249360e59e1/ymusic/5dde/f2d3/3dba/0a0914dcd1585247b58938a3a7afd925.mp3',
+        cover: 'https://ww1.sinaimg.cn/bmiddle/006AU3yKly1fvrd0yd3csj30ij0rswgx.jpg',
       },
       {
         name: '9420',
